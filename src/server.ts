8d9d5319f3cbc2d08e9cec47cbf6e3396f9c38b5
saferboy <muxtordev@gmail.com>
2023-06-16 15:19:39 +0500

    add middleware ko'rinishi

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import sharp from 'sharp'
 import path from 'path'
 
 const uploadPath = path.join(__dirname, '../', 'uploads')
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, uploadPath)
@@ -15,7 +16,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage })
 
 function resizeImageMiddleware(req: Request, res: Response, next: NextFunction) {
-    let compressedImageFileSavePath = path.join(__dirname, '../', 'public', 'thumbnail', `${new Date().getTime()}-640x480` + ".jpg")
+    let compressedImageFileSavePath = path.join(__dirname, '../', 'uploads', 'thumbnail', `${new Date().getTime()}-640x480` + ".jpg")
 
     sharp(req.file?.path).resize(640, 480).jpeg({
         quality: 80,
@@ -29,6 +30,32 @@ function resizeImageMiddleware(req: Request, res: Response, next: NextFunction) 
         next(); // next() ni chaqirish
     });
 }
+
+// Middleware ko'rinishi
+// export function resizeImage(req: Request, res: Response, next: NextFunction) {
+
+//     const width = 640
+//     const height = 480
+
+//     let compressedFileName = `/thumbnail/${new Date().getTime()}-${width}x${height}.jpg`
+
+//     let compressedImageFileSavePath = path.join(__dirname, '../../', 'upload', compressedFileName)
+
+//     sharp(req.file?.path).resize(width, height).jpeg({
+//         quality: 80,
+//         chromaSubsampling: '4:4:4',
+//     }).toFile(compressedImageFileSavePath, (err, info) => {
+//         if (err) {
+//             res.send(err)
+//         } else {
+//             res.locals.originalFile = req.file
+//             res.locals.compressInfo = info
+
+//             next()
+//         }
+//         next(err); // next() ni chaqirish
+//     });
+// }
 
 ////////////////////////////////////////////////////////
 // For example
